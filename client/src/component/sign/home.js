d8c07bdfdeb92b5d0447342c11ae25ebc55e9c2c
Kushagra Anand <kushrokz95@gmail.com>
2020-09-15 22:08:42 +0530

    first commit

---
+++ client/src/component/sign/home.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+
+const Home = () => {
+  return <div>fgvgregrgr</div>;
+};
+
+export default Home;
